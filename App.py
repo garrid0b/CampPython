9a7babb6f2cba7ec6c204ca9965efde31f6d761d
Andres Garrido <andresrosmila@gmail.com>
2022-11-16 22:05:49 -0500

    cambio final

--- App.py
+++ App.py
@@ -1,12 +1,6 @@
 # Escribe un programa en la consola de python que pida al usuario su peso (en kg) y estatura (en metros),
 #  calcule el índice de masa corporal y lo almacene en una variable, e imprima por pantalla la frase 
 # (Tu índice de masa corporal es donde es el índice de masa corporal) calculado redondeado con dos decimales. 
-
-
-
-
-
-
 print('Hola, por favor ingresa tu estatura en Metros')
 estatura=input()
 estatura=float(estatura)
